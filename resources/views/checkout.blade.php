7a9666190cd09f3c7fdadba8100358b5d872bc7e
ANonymous <postmaster.richmama@gmail.com>
2019-10-24 02:33:43 +0100

    more on checkout.. billing info

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -11,11 +11,12 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-8 order-2 order-lg-1">
-					<form class="checkout-form">
-						<div class="cf-title">Billing Address</div>
+					<form class="checkout-form" id="checkout-form">
+					    {!! csrf_field() !!}
+						<div class="cf-title">Billing Info</div>
 						<div class="row">
 							<div class="col-md-7">
-								<p>*Billing Information</p>
+								<p>*Confirm Your Billing Information</p>
 							</div>
 							<div class="col-md-5">
 								<div class="cf-radio-btns address-rb">
@@ -31,17 +32,61 @@
 							</div>
 						</div>
 						<div class="row address-inputs">
+							<div class="col-md-6">
+                                        <input type="text" class="form-control" id="first_name" name="fname" value="{{$user->fname}}" data-default="{{$user->fname}}" placeholder="First Name" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" id="last_name" name="lname" value="{{$user->lname}}" data-default="{{$user->lname}}" placeholder="Last Name" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" id="company" name="company" placeholder="Company Name" value="{{$sd['company']}}" data-default="{{$sd['company']}}">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="{{$user->email}}" data-default="{{$user->email}}">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <select class="form-control w-100" id="country" style="margin-bottom: 10px;">
+                                           <option value="ng">Nigeria</option>
+                                        </select>
+                                    </div>
+                                    
+						</div>
+						<div class="row address-inputs">
 							<div class="col-md-12">
-								<input type="text" placeholder="Address">
-								<input type="text" placeholder="Address line 2">
-								<input type="text" placeholder="Country">
+								 <input type="text" class="form-control" id="street_address" name="address" placeholder="Address" value="{{$sd['address']}}" data-default="{{$sd['address']}}">
 							</div>
 							<div class="col-md-6">
-								<input type="text" placeholder="Zip code">
+								   <input type="text" class="form-control" id="city" name="city" placeholder="City" value="{{$sd['city']}}" data-default="{{$sd['city']}}">
 							</div>
 							<div class="col-md-6">
-								<input type="text" placeholder="Phone no.">
+                                        <select class="form-control w-100" name="state" style="margin-bottom: 10px;">
+                                        <option value="none">Select state</option>
+                                        <?php 
+                                          foreach($states as $key => $value){
+                                          	$selectedText = ($key == $sd['state']) ? "selected='selected'" : "";                                           
+                                        ?>
+                                        <option value="<?=$key?>" <?=$selectedText?> ><?=$value?></option>
+                                        <?php 
+                                          }
+                                        ?>
+                                    </select>
+									</div>
+							<div class="col-md-6">
+								  <input type="text" class="form-control" id="zipCode" name="zip" placeholder="Zip Code" value="{{$sd['zipcode']}}" data-default="{{$sd['zipcode']}}">
 							</div>
+							<div class="col-md-6">
+                                <input type="text" class="form-control" id="phone_number" name="phone" min="0" placeholder="Phone No" value="{{$user->phone}}" data-default="{{$user->phone}}">
+                            </div>
+							<div class="col-md-12">
+                                        <div class="custom-control custom-checkbox d-block mb-2">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck2" name="terms">
+                                            <label class="custom-control-label" for="customCheck2">I accept <a href="#">terms and conditions</a></label>
+                                        </div>
+                                        <div class="custom-control custom-checkbox d-block">
+                                            <input type="checkbox" class="custom-control-input" name="ssa" id="customCheck3">
+                                            <label class="custom-control-label" for="customCheck3">Save this address</label>
+                                        </div>
+                            </div>
 						</div>
 						<div class="cf-title">Delievery Info</div>
 						<div class="row shipping-btns">
