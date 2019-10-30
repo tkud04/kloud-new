b1ebbf5dc30e15e32e2ca1aa437cc8ade6a0671d
Tobi K <32499735+tkud04@users.noreply.github.com>
2019-10-30 17:19:04 +0100

    Swal input errors

--- resources/views/input-errors.blade.php
+++ resources/views/input-errors.blade.php
@@ -1,11 +1,12 @@
- <div class="row">
-                      <div class="col-md-12">
-                   <div class="alert alert-danger" role="alert">
-                       <p><strong>Whoops!</strong> There were some problems with your input.</p>
-                       <button class="close" data-dismiss="alert">x</button>
-                       <div class="clearfix"></div>
-                       <br><br>
-                       <ul>                       	
+ 
+<script>
+let text = 
+Swal.fire({
+  position: 'top-end',
+  type: 'error',
+  title: '<strong>Whoops!</strong> There were some problems with your input.',
+  text: `
+   <ul>                       	
                        	@foreach ($errors->all() as $error)
                               @if($error == "The g-recaptcha-response field is required.")
 							    <li>You must fill the captcha to continue.</li>
@@ -16,6 +17,9 @@
 						      @endif
 						   @endforeach
                        </ul>
-                     </div>
-                   </div>
-                 </div>
+         `,
+  showConfirmButton: false,
+  timer: 2500
+});
+</script>
+	
