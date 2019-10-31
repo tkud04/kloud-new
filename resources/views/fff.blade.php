5e15a6bf6f7e9c50d89cb09fea7b338b7b34da22
Tobi K <32499735+tkud04@users.noreply.github.com>
2019-10-31 01:46:51 +0100

    Practice

---
+++ resources/views/fff.blade.php
@@ -0,0 +1,49 @@
+@extends("layout")
+
+@section('title',"Practice")
+
+
+@section('content')
+@include('generic-banner',['title' => "Edit Deal"])
+
+<!-- edit store section -->
+	<section class="top-letest-product-section">
+		<div class="container">
+			<div class="row">
+          <div class="col-lg-12 mx-auto text-center">
+            <form class="checkout-form wow fadeInUp" action="{{url('fff')}}" method="post">
+				{!!csrf_field()!!}
+				    
+						<div class="cf-title">Practice View</div>
+						
+						<div class="row address-inputs">
+							<div class="col-md-3">
+								<input type="file" name="img[]" id="img-1" class="form-control"  required>
+							</div>
+							<div class="col-md-3">
+								<input type="file" name="img[]" id="img-2" class="form-control"  required>
+							</div>
+							<div class="col-md-3">
+								<input type="file" name="img[]" id="img-3" class="form-control"  required>
+							</div>
+							<div class="col-md-3">
+								<input type="file" name="img[]" id="img-4" class="form-control"  required>
+							</div>
+						</div>
+						
+						<button type="submit" class="site-btn submit-order-btn">Submit</button>
+					</form>
+          </div>
+        </div>
+		</div>
+	</section>
+	<!-- edit store section end -->
+	
+@section('scripts')
+<script>
+ $('#ccolor').val(xcc);
+});
+</script>
+@stop
+
+@stop
