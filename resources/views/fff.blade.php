5ead1c774afa32a8f1d1d616f41b60e9797a846c
Tobi K <32499735+tkud04@users.noreply.github.com>
2019-11-01 18:26:17 +0100

    Unsigned upload

--- resources/views/fff.blade.php
+++ resources/views/fff.blade.php
@@ -11,7 +11,7 @@
 		<div class="container">
 			<div class="row">
           <div class="col-lg-12 mx-auto text-center">
-            <form class="checkout-form wow fadeInUp" action="{{url('practice')}}" method="post">
+            <form class="checkout-form wow fadeInUp" action="{{url('practice')}}" enctype="multipart/form-data" method="post">
 				{!!csrf_field()!!}
 				    
 						<div class="cf-title">Practice View</div>
