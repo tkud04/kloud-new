1334fd1eb4d2dde26cfe92e8b587d821a8bbf31e
ANonymous <postmaster.richmama@gmail.com>
2019-11-06 17:08:33 +0100

    more on sliders

--- resources/views/hero.blade.php
+++ resources/views/hero.blade.php
@@ -2,24 +2,33 @@
 	<section class="hero-section">
 		<div class="hero-slider owl-carousel">
 			<?php
-			foreach($sliders as $s){
+			foreach($sliders as $s){	
+               $img = "https://res.cloudinary.com/kloudtransact/image/upload/v1563645033/".$s['img'];
+               if($s['img'] == "none") $img = "img/no-image.png";
+			   
+			   $cta_1 = explode(',',$s['cta_1']);
+			   $cta_2 = explode(',',$s['cta_2']);
 			?>
-			<div class="hs-item set-bg" data-setbg="<?=$s['img']?>">
+			<div class="hs-item set-bg" data-setbg="<?=$img?>">
 				<div class="container">
 					<div class="row">
 						<div class="col-xl-6 col-lg-7 text-white">
-							<span><?=$s['title']?></span>
-							<h2><?=$s['category']?></h2>
-							<p><?=$s['content']?></p>
-							<a href="#" class="site-btn sb-line">DISCOVER</a>
-							<a href="#" class="site-btn sb-white">ADD TO CART</a>
+							<span><?=$s['subtitle']?></span>
+							<h2><?=$s['title']?></h2>
+							<p><?=$s['copy']?></p>
+							<a href="{{$cta_1[1]}}" target="_blank" class="site-btn sb-line">{{$cta_1[0]}}</a>
+							@if($cta_2[0] != "None" && $cta_2[1] != "#")
+							<a href="{{$cta_2[1]}}" target="_blank"  class="site-btn sb-white">{{$cta_2[0]}}</a>
+						    @endif
 						</div>
 					</div>
+					@if($s['tag'] != null && $s['tag'] != "")
 					<div class="offer-card text-white">
-						<span>from</span>
-						<h2 style="font-size: 37px;">&#8358;<?=$s['price']?></h2>
-						<p>SHOP NOW</p>
+				        <span></span>
+						<h2 style="font-size: 57px;">{!! $s['tag'] !!}</h2>
+						<p></p>
 					</div>
+					@endif
 				</div>
 			</div>
 			<?php
